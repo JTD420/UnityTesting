c140fb668eed3dfa359b13267fabacf088f4d2e6
Brad <jakethedog420@protonmail.com>
2021-09-06 22:09:18 +0100

    Initial Upload

---
+++ Assets/Models/tile v1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tile v1" tilewidth="220" tileheight="104" tilecount="1" columns="1" objectalignment="topright">
+ <grid orientation="isometric" width="220" height="104"/>
+ <image source="tile v1.png" width="220" height="104"/>
+</tileset>
